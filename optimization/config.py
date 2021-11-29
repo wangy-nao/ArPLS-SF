471472138611c2da929b9837ad83c031f32f3396
wangy-nao <wangy@nao.cas.cn>
2021-11-29 15:30:40 +0800

    change the config

--- optimization/config.py
+++ optimization/config.py
@@ -1,12 +1,15 @@
-# config the parameters for ArPLS-SF, the sample time for FAST pulsar backend is about 49us and the frequency resolution is 122.07KHz
+# config the parameters for ArPLS-SF
+# the sample time for FAST pulsar backend is about 49us 
+# the frequency resolution is 122.07KHz
+
 debug = False
-t_sample = int(32)     #number of time intervals to construct the filter, about 0.4s
-f_sample = int(4096)   #number of freq channels
-subint= int(256)         #subint number, depends on the PSRFITS data
-beam = 19                #beam number to process
-path2save = 'D:\\pynote\\ArPLS-SF-results\\J0528+2200'
-#path2save = 'D:\\ArPLS-SF-results\\'
-pol_num = 0               #0,1,2,3,4 which pol to process
-t_shape = int(256)     # parameters to construct the filter, about 50ms
-f_shape = int(4096)
-factor = 1.5             #setting threshold 1,2,3 sigma
+t_sample = int(64)      #number of time intervals to construct the filter, about 0.4s
+f_sample = int(4096)    #number of freq channels to construct the filter
+subint= int(256)        #subint number, depends on the PSRFITS data
+beam = 19               #beam number to process, for FAST it's 19
+pol_num = 0             #0,1,2,3 which pol to process
+t_shape = int(1024)     # number of time intervals to use the filter, about 50ms
+f_shape = int(4096)     # number of freq channels to use the filter
+factor = 1.5            #setting threshold
+#path2save = 'D:\\pynote\\ArPLS-SF-results\\J1855+0455\\'
+path2save = 'D:\\pynote\\ArPLS-SF-results\\J0528+2200\\'
